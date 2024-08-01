ae84e997f636bb090b20d19c3a0c9d0bcb7529b2
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-01 11:18:50 +0100

    chore: apply rfc 166 formatting

--- flake.nix
+++ flake.nix
@@ -5,14 +5,20 @@
     let
       systems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      });
+    in {
+      legacyPackages = forAllSystems (system:
+        import ./default.nix {
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        });
 
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      packages = forAllSystems (system:
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v)
+        self.legacyPackages.${system});
 
-      defaultPackage = forAllSystems (system: self.packages.${system}.xivlauncher-rb);
+      defaultPackage =
+        forAllSystems (system: self.packages.${system}.xivlauncher-rb);
     };
 }
--- pkgs/xivlauncher-rb/default.nix
+++ pkgs/xivlauncher-rb/default.nix
@@ -1,92 +1,91 @@
-{ lib, buildDotnetModule, fetchFromGitHub, dotnetCorePackages, SDL2, libsecret, glib, gnutls, aria2, steam, gst_all_1
-, copyDesktopItems, makeDesktopItem, makeWrapper
-, useSteamRun ? true
-, useGameMode ? false
-}:
+{ lib, buildDotnetModule, fetchFromGitHub, dotnetCorePackages, SDL2, libsecret
+, glib, gnutls, aria2, steam, gst_all_1, copyDesktopItems, makeDesktopItem
+, makeWrapper, useSteamRun ? true, useGameMode ? false }:
 
-let
-  tag = "1.1.0.6";
-in
-  buildDotnetModule rec {
-    pname = "xivlauncher-rb";
-    version = tag;
+let tag = "1.1.0.6";
+in buildDotnetModule rec {
+  pname = "xivlauncher-rb";
+  version = tag;
 
-    src = fetchFromGitHub {
-      owner = "rankynbass";
-      repo = "XIVLauncher.Core";
-      rev = "rb-v${tag}";
-      hash = "sha256-ptpQnWC7aYmC0apwMWTCAQuyG7vUF50fERiXFWHrxg4=";
-      fetchSubmodules = true;
-    };
+  src = fetchFromGitHub {
+    owner = "rankynbass";
+    repo = "XIVLauncher.Core";
+    rev = "rb-v${tag}";
+    hash = "sha256-ptpQnWC7aYmC0apwMWTCAQuyG7vUF50fERiXFWHrxg4=";
+    fetchSubmodules = true;
+  };
 
-    nativeBuildInputs = [ copyDesktopItems makeWrapper ];
+  nativeBuildInputs = [ copyDesktopItems makeWrapper ];
 
-    buildInputs = with gst_all_1; [ gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav ];
+  buildInputs = with gst_all_1; [
+    gstreamer
+    gst-plugins-base
+    gst-plugins-good
+    gst-plugins-bad
+    gst-plugins-ugly
+    gst-libav
+  ];
 
-    projectFile = "src/XIVLauncher.Core/XIVLauncher.Core.csproj";
-    nugetDeps = ./deps.nix; # File generated with `nix-build -A xivlauncher-rb.passthru.fetch-deps`
+  projectFile = "src/XIVLauncher.Core/XIVLauncher.Core.csproj";
+  nugetDeps =
+    ./deps.nix; # File generated with `nix-build -A xivlauncher-rb.passthru.fetch-deps`
 
-    # please do not unpin these even if they match the defaults, xivlauncher is sensitive to .NET versions
-    dotnet-sdk = dotnetCorePackages.sdk_8_0;
-    dotnet-runtime = dotnetCorePackages.runtime_8_0;
+  # please do not unpin these even if they match the defaults, xivlauncher is sensitive to .NET versions
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-    dotnetFlags = [
-      "-p:BuildHash=${tag}"
-      "-p:PublishSingleFile=false"
-    ];
+  dotnetFlags = [ "-p:BuildHash=${tag}" "-p:PublishSingleFile=false" ];
 
-    postPatch = ''
-      substituteInPlace lib/FFXIVQuickLauncher/src/XIVLauncher.Common/Game/Patch/Acquisition/Aria/AriaHttpPatchAcquisition.cs \
-        --replace-fail 'ariaPath = "aria2c"' 'ariaPath = "${aria2}/bin/aria2c"'
-    '';
+  postPatch = ''
+    substituteInPlace lib/FFXIVQuickLauncher/src/XIVLauncher.Common/Game/Patch/Acquisition/Aria/AriaHttpPatchAcquisition.cs \
+      --replace-fail 'ariaPath = "aria2c"' 'ariaPath = "${aria2}/bin/aria2c"'
+  '';
 
-    postInstall = ''
-      mkdir -p $out/share/pixmaps
-      cp src/XIVLauncher.Core/Resources/logo.png $out/share/pixmaps/xivlauncher.png
-    '';
+  postInstall = ''
+    mkdir -p $out/share/pixmaps
+    cp src/XIVLauncher.Core/Resources/logo.png $out/share/pixmaps/xivlauncher.png
+  '';
 
-    postFixup = lib.optionalString useSteamRun (let
-      steam-run = (steam.override {
-        extraPkgs = pkgs: [
-          pkgs.libunwind
-          pkgs.zstd
-        ] ++ lib.optional useGameMode pkgs.gamemode;
-        extraProfile = ''
-          unset TZ
-        '';
-      }).run;
-    in ''
-      substituteInPlace $out/bin/XIVLauncher.Core \
-        --replace 'exec' 'exec ${steam-run}/bin/steam-run'
-    '') + ''
-      wrapProgram $out/bin/XIVLauncher.Core --prefix GST_PLUGIN_SYSTEM_PATH_1_0 ":" "$GST_PLUGIN_SYSTEM_PATH_1_0"
-      # the reference to aria2 gets mangled as UTF-16LE and isn't detectable by nix: https://github.com/NixOS/nixpkgs/issues/220065
-      mkdir -p $out/nix-support
-      echo ${aria2} >> $out/nix-support/depends
-    '';
+  postFixup = lib.optionalString useSteamRun (let
+    steam-run = (steam.override {
+      extraPkgs = pkgs:
+        [ pkgs.libunwind pkgs.zstd ] ++ lib.optional useGameMode pkgs.gamemode;
+      extraProfile = ''
+        unset TZ
+      '';
+    }).run;
+  in ''
+    substituteInPlace $out/bin/XIVLauncher.Core \
+      --replace 'exec' 'exec ${steam-run}/bin/steam-run'
+  '') + ''
+    wrapProgram $out/bin/XIVLauncher.Core --prefix GST_PLUGIN_SYSTEM_PATH_1_0 ":" "$GST_PLUGIN_SYSTEM_PATH_1_0"
+    # the reference to aria2 gets mangled as UTF-16LE and isn't detectable by nix: https://github.com/NixOS/nixpkgs/issues/220065
+    mkdir -p $out/nix-support
+    echo ${aria2} >> $out/nix-support/depends
+  '';
 
-    executables = [ "XIVLauncher.Core" ];
+  executables = [ "XIVLauncher.Core" ];
 
-    runtimeDeps = [ SDL2 libsecret glib gnutls ];
+  runtimeDeps = [ SDL2 libsecret glib gnutls ];
 
-    desktopItems = [
-      (makeDesktopItem {
-        name = "xivlauncher-rb";
-        exec = "XIVLauncher.Core";
-        icon = "xivlauncher";
-        desktopName = "XIVLauncher-RB";
-        comment = meta.description;
-        categories = [ "Game" ];
-        startupWMClass = "XIVLauncher.Core";
-      })
-    ];
+  desktopItems = [
+    (makeDesktopItem {
+      name = "xivlauncher-rb";
+      exec = "XIVLauncher.Core";
+      icon = "xivlauncher";
+      desktopName = "XIVLauncher-RB";
+      comment = meta.description;
+      categories = [ "Game" ];
+      startupWMClass = "XIVLauncher.Core";
+    })
+  ];
 
-    meta = with lib; {
-      description = "Custom launcher for FFXIV";
-      homepage = "https://github.com/rankynbass/XIVLauncher.Core";
-      license = licenses.gpl3;
-#      maintainers = with maintainers; [ sersorrel witchof0x20 ];
-      platforms = [ "x86_64-linux" ];
-      mainProgram = "XIVLauncher.Core";
-    };
-  }
+  meta = with lib; {
+    description = "Custom launcher for FFXIV";
+    homepage = "https://github.com/rankynbass/XIVLauncher.Core";
+    license = licenses.gpl3;
+    #maintainers = with maintainers; [ sersorrel witchof0x20 ];
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "XIVLauncher.Core";
+  };
+}
