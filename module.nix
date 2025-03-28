2ae73c4cab129f052101cc9e63f3554a6e7f48e9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-03-28 18:52:05 +0000

    Add a module for enabling XIVLauncher

--- module.nix
+++ module.nix
@@ -1,5 +1,27 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   nixpkgs.overlays = [
     (import ./overlay.nix)
   ];
+
+  options.programs.xivlauncher.enable = lib.mkEnableOption "Enable XIVLauncher";
+
+  config = lib.mkIf config.programs.xivlauncher.enable {
+    environment.systemPackages = [
+      pkgs.xivlauncher-rb.override
+      {
+        useGameMode = config.services.gamemode.enable;
+        nvngxPath =
+          if builtins.elem "nvidia" config.services.xserver.videoDrivers == true then
+            "${config.hardware.nvidia.package}/lib/nvidia/wine"
+          else
+            "";
+      }
+    ];
+  };
 }
