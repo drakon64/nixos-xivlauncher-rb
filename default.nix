5b3428e9591be4b456c6ad173922c487f5a19462
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-03-28 18:51:52 +0000

    Reuse `flake.lock` in `default.nix`

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ?
+    let
+      lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    in
+    (import (fetchTarball {
+      url = "https://github.com/${lock.owner}/${lock.repo}/archives/refs/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    }) { }),
 }:
 {
   xivlauncher-rb = pkgs.callPackage ./xivlauncher-rb { };
